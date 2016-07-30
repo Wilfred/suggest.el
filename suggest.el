d0723b9c1ecf819f5d28f7c5bc63807c059ac4b0
Hariharan R <hariharanrangasamy@gmail.com>
2016-07-30 23:17:11 +0530

    add f.el in package requires before requiring f
    fixes no such file or directory f

--- suggest.el
+++ suggest.el
@@ -5,7 +5,7 @@
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Version: 0.1
 ;; Keywords: convenience
-;; Package-Requires: ((loop "1.3") (dash "2.12.0") (s "1.11.0"))
+;; Package-Requires: ((loop "1.3") (dash "2.12.0") (s "1.11.0") (f "0.18.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
