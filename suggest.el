4829536807053bdb412ed568cbe6b5f083ae5f32
Nick Drozd <nicholasdrozd@gmail.com>
2017-10-04 22:34:51 -0500

    Add boolean functions

--- suggest.el
+++ suggest.el
@@ -40,6 +40,12 @@
 (defvar suggest-functions
   (list
    ;; TODO: add funcall, apply and map?
+   ;; Boolean functions
+   #'not
+   #'booleanp
+   #'consp
+   #'numberp
+   #'stringp
    ;; Built-in functions that access or examine lists.
    ;; TODO: why isn't car marked as pure?
    #'car
@@ -634,7 +640,7 @@ This is primarily for quoting symbols."
     ;; See if (func value1 value2...) gives us a value.
     (-when-let (result (suggest--call func input-values input-literals))
       (push result outputs))
-    
+
     ;; See if (apply func input-values) gives us a value.
     (when (and (eq (length input-values) 1) (listp (car input-values)))
       (-when-let (result (suggest--call func input-values input-literals t))
@@ -699,7 +705,7 @@ than their values."
                        (cl-incf possibilities-count)
                        (when (>= possibilities-count suggest--max-possibilities)
                          (throw 'done nil))
-                       
+
                        ;; If we're on the first iteration, we're just
                        ;; searching all input permutations. Don't try any
                        ;; other permutations, or we end up showing e.g. both
@@ -794,7 +800,7 @@ than their values."
     (setq possibilities
           (-take 5
                  (-sort #'suggest--cmp-relevance possibilities)))
-    
+
     (if possibilities
         (suggest--write-suggestions
          possibilities
